29642bf9b908a9013eff3b504e0469fbc92654ff
Kyle Greene <kylengreene@gmail.com>
2020-04-13 10:41:46 -0500

    final set up

--- database.sql
+++ database.sql
@@ -3,11 +3,14 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+-- name db 'otto_database'
 
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (100) UNIQUE NOT NULL,
     "password" VARCHAR (100) NOT NULL,
+    "first_name" VARCHAR (100) NOT NULL,
+    "last_name" VARCHAR (100) NOT NULL,
     "user_type" INTEGER,
     "phone_number" INTEGER,
     "approved_user" BOOLEAN default false
@@ -17,7 +20,12 @@ CREATE TABLE "property" (
     "id" SERIAL PRIMARY KEY,
     "user_id" INTEGER REFERENCES "user" NOT NULL,
     "active" BOOLEAN default false NOT NULL,
-    "address" VARCHAR (200) NOT NULL,
+    "street_number" VARCHAR (200) NOT NULL,
+    "street_name" VARCHAR (200) NOT NULL,
+    "unit_number" VARCHAR (200) DEFAULT NULL,
+    "state" VARCHAR (200) NOT NULL,
+    "city" VARCHAR (200) NOT NULL,
+    "zip_code" INTEGER NOT NULL,
     "property_type" VARCHAR (100),
     "net_operating_income" INTEGER,
     "gross_income" INTEGER,
@@ -25,20 +33,14 @@ CREATE TABLE "property" (
     "desired_price" INTEGER
 );
 
-CREATE TABLE "searches" (
+CREATE TABLE "search" (
     "id" SERIAL PRIMARY KEY,
     "user_id" INTEGER REFERENCES "user" NOT NULL,
     "search_string" VARCHAR (100) NOT NULL
 );
 
-CREATE TABLE "interests" (
+CREATE TABLE "interest" (
     "id" SERIAL PRIMARY KEY,
     "user_id" INTEGER REFERENCES "user" NOT NULL,
     "property_id" INTEGER REFERENCES "property" NOT NULL
 );
-=======
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
-
-
