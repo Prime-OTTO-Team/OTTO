34a9054f27969d98e1515b9a3d4a1feb72d5b22a
Derek Boat <derek.boat@gmail.com>
2020-04-14 10:31:50 -0500

    added conditional rendering code

--- src/components/AccountPage/AccountPage.js
+++ src/components/AccountPage/AccountPage.js
@@ -2,10 +2,30 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 class AccountPage extends Component {
+    state = { 
+        listing = TRUE
+    }
+
+    handleClick = () => {
+        this.state.listing = TRUE
+    }
+    
+    handleClick2 = () => {
+        this.state.listing = FALSE
+    }
+
     render() {
         return (
             <div className='container'>
                 <h1>AccountPage</h1>
+                <button onClick={this.handleClick}>My Listings</button> 
+                <button onClick={this.handleClick2}>My Favorites</button>
+            <div classname='listings'> 
+            Here is where listings go.
+            {this.state.listing ? (<p>listings</p>):(<p>favorites</p>)}
+            </div>
+
+
             </div>
         )
     }
