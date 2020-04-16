5f1df3bdf91c84dde9b2394b64c9dc7093ce7ebe
Matt Kimlinger <Mkimlinger123@gmail.com>
2020-04-16 11:44:28 -0500

    geocode function made

--- src/components/GoogleMaps/GoogleMap.js
+++ src/components/GoogleMaps/GoogleMap.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
-
-
-const mapStyles = {
-    width: '50%',
-    height: '50%wh'
-};
+import axios from 'axios';
 
 class GoogleMap extends Component {
+    geocode = async (address) => {
+        try {
+            const rawResponse = await axios.get(
+                'https://maps.googleapis.com/maps/api/geocode/json', {
+                params: {
+                    address: address,
+                    key: ''
+                }
+            }
+            )
+            console.log('geocode location: ', rawResponse.data.results[0].geometry.location);
+        } catch (error) {
+            //the only time this error will occur is if the key is invalid
+            console.log('geocode error: ', error);
+        }
+    }
     render() {
         // console.log('process.env', process.env)
         // console.log('API KEY: ', process.env.GOOGLE_MAPS_API_KEY);
+        this.geocode('55109');
         return (
             <Map
                 google={this.props.google}
@@ -28,6 +40,12 @@ class GoogleMap extends Component {
 // const mapStateToProps = (reduxState) => ({
 //     reduxState
 // })
+
+const mapStyles = {
+    width: '50%',
+    height: '50%wh'
+};
+
 
 export default GoogleApiWrapper({
     //during development this api key will be destroyed  until a proper method is implemented
