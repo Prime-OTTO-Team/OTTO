7510a7434042403a7e1102e7a991edda28c59132
ErwinZadeh <farshidmzadeh@icloud.com>
2020-04-16 01:25:38 -0500

    sticky footer and its basic styling with material-UI was done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './Footer.css'
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import './Footer.css'
+// import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
 
 
 
-const Footer = () => (
-  <footer>
-    &copy; Mktfare
-    <div className="nav-right">
-    <Link className="nav-link" to="/about"> About </Link>
-    <Link className="nav-link" to="/contact"> Contact Us </Link>
-    <Link className="nav-link" to="/terms"> Terms and Conditions </Link>
-    </div>
-  </footer>
-);
+// const Footer = () => (
+//   <footer>
+//     &copy; Mktfare
+//     <div className="nav-right">
+//     <Link className="nav-link" to="/about"> About </Link>
+//     <Link className="nav-link" to="/contact"> Contact Us </Link>
+//     <Link className="nav-link" to="/terms"> Terms and Conditions </Link>
+//     </div>
+//   </footer>
+// );
 
-export default Footer;
+// export default Footer;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ import './Nav.css';
 const Nav = (props) => (
   <div className="nav">
     <Link to="/home">
-      <h2 className="nav-title">OTTO Project</h2>
+      <h2 className="nav-title">OTTO</h2>
     </Link>
     <div className="nav-right">
       <Link className="nav-link" to="/home"
