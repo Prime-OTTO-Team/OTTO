f25362e6a39b03f7371c144fba66b0c384b67fba
Matt Kimlinger <Mkimlinger123@gmail.com>
2020-04-17 12:41:17 -0500

    Adds marker locations on google map.

--- src/components/GoogleMaps/GoogleMap.js
+++ src/components/GoogleMaps/GoogleMap.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
-import axios from 'axios';
-
+import '../LandingPage/LandingPage.css'
 class GoogleMap extends Component {
     state = {
-        showingInfoWindow: false,  //Hides or the shows the infoWindow
-        activeMarker: {},          //Shows the active marker upon click
-        selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+        showingInfoWindow: false, 
+        activeMarker: {},          
+        selectedPlace: {} 
     };
+    componentDidMount () {
+        // this.addMarkers()   
+    }
     onMarkerClick = (props, marker, e) =>
         this.setState({
             selectedPlace: props,
             activeMarker: marker,
             showingInfoWindow: true
         });
-
     onClose = props => {
         if (this.state.showingInfoWindow) {
             this.setState({
@@ -24,39 +24,27 @@ class GoogleMap extends Component {
             });
         }
     };
-    componentDidMount() {
-        this.getPropertyLocations();
-    }
-    getPropertyLocations = () => {
-        const markerLocations = this.props.markerLocations;
-        if (!markerLocations) {
-            return console.log('marker locations undefined');
-        } else {
-            return markerLocations;
-        }
-    }
-    addMarkers = () => {
-
-    }
-    
     render() {
-        // console.log('process.env', process.env)
-        // console.log('API KEY: ', process.env.GOOGLE_MAPS_API_KEY);
         return (
-            <Map
-                google={this.props.google}
-                zoom={14}
-                style={mapStyles}
-                initialCenter={{
-                    lat: 44.9778,
-                    lng: -93.2650
-                }}
-            >
-                <Marker
+            <div className="mapContainer">
+                <Map
+                    google={this.props.google}
+                    zoom={10}
+                    style={mapStyles}
+                    initialCenter={{
+                        lat: 44.9778,
+                        lng: -93.2650
+                    }}
                 >
-
-                </Marker>
-            </Map>
+                    {this.props.markerLocations.map((marker) => {
+                        console.log('marker: ', marker);
+                            return (
+                                <Marker position={{lat: marker.lat, lng: marker.lng }} />
+                            )
+                        })
+                    }
+                </Map>
+            </div>
         )
     }
 }
@@ -65,13 +53,11 @@ class GoogleMap extends Component {
 // })
 
 const mapStyles = {
-    width: '50%',
-    height: '30px'
+    width: '800px',
+    height: '800px',
 };
 
-
 export default GoogleApiWrapper({
-    //during development this api key will be destroyed  until a proper method is implemented
     //TODO: 
     apiKey: 'AIzaSyCHElb_DfSY05GT5sQL4K_8PU8fWIE--xo'
     // apiKey: `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
