c256937418031b7dcffecde33bed984e3548bb26
Kyle Greene <kylengreene@gmail.com>
2020-04-23 10:51:14 -0500

    restructured account saga

--- server/routes/account.router.js
+++ server/routes/account.router.js
@@ -2,10 +2,11 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/property/:id', (req, res) => {
-    console.log("in getAccount route", req.params);
+router.get('/property', (req, res) => {
+    const userId = req.user.id;
+    console.log("in getAccount/porperty route with userId", userId);
     let queryString = `SELECT * FROM "property" WHERE "user_id" = $1 AND "active"= 'TRUE' ORDER BY "id" ASC`;
-    pool.query(queryString, [req.params.id])
+    pool.query(queryString, [userId])
         .then(results => {
             res.send(results.rows);
         }).catch(error => {
@@ -13,13 +14,27 @@ router.get('/property/:id', (req, res) => {
             res.sendStatus(500);
         });
 });
-
-router.get('/interest/:id', (req, res) => {
-    console.log("in getAccountInterests route", req.params);
+router.get('/favorite', (req, res) => {
+    const userId = req.user.id;
+    console.log("in getAccount/favorite route with userId", userId);
+    let queryString = `SELECT "favorite"."id", "favorite"."user_id", "active", "address", "unit_number", "state", "city", "zip_code", "property_type",
+     "net_operating_income", "gross_income", "gross_expense", "desired_price" FROM "property" JOIN "favorite" ON "property"."id"="favorite"."property_id" 
+     JOIN "user" ON "favorite"."user_id"= "user"."id" WHERE "user"."id"=$1 ORDER BY "id" ASC`;
+    pool.query(queryString, [userId])
+        .then(results => {
+            res.send(results.rows);
+        }).catch(error => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+});
+router.get('/interest', (req, res) => {
+    const userId = req.user.id;
+    console.log("in getAccountInterests route with userId", userId);
     let queryString = `SELECT "interest"."id", "interest"."user_id", "active", "address", "unit_number", "state", "city", "zip_code", "property_type",
      "net_operating_income", "gross_income", "gross_expense", "desired_price" FROM "property" JOIN "interest" ON "property"."id"="interest"."property_id" 
      JOIN "user" ON "interest"."user_id"= "user"."id" WHERE "user"."id"=$1 ORDER BY "id" ASC`;
-    pool.query(queryString, [req.params.id])
+    pool.query(queryString, [userId])
         .then(results => {
             res.send(results.rows);
         }).catch(error => {
@@ -30,7 +45,7 @@ router.get('/interest/:id', (req, res) => {
 
 router.delete('/interest/:id', (req, res) => {
     console.log("in deleteAccountInterest route", req.params);
-    let queryString = `DELETE FROM "interest" WHERE "id" = $1`;
+    let queryString = `DELETE FROM "favorite" WHERE "id" = $1`;
     pool.query(queryString, [req.params.id])
         .then(() => res.sendStatus(200))
         .catch(() => res.sendStatus(500));
--- server/routes/interest.router.js
+++ server/routes/interest.router.js
@@ -33,6 +33,7 @@ router.post('/', async (req, res) => {
 });
 
 router.get('/favoritesAndInterests', async (req, res) => {
+    console.log('logging req.user', req.user);
     const userId = req.user.id;
     const propertyId = Number(req.query.propertyId);
     console.log('user.id: ', userId);
@@ -41,14 +42,18 @@ router.get('/favoritesAndInterests', async (req, res) => {
         return res.sendStatus(401);
     }
     try {
-        const propertyInterestsQuery = `SELECT * FROM favorite WHERE "user_id" = $1;`;
-        const interestsQuery = `SELECT * FROM interest WHERE "user_id" = $1;`;
+        const favoritesQuery = `SELECT "favorite"."id", "favorite"."user_id", "active", "address", "unit_number", "state", "city", "zip_code", "property_type",
+     "net_operating_income", "gross_income", "gross_expense", "desired_price" FROM "property" JOIN "favorite" ON "property"."id"="favorite"."property_id" 
+     JOIN "user" ON "favorite"."user_id"= "user"."id" WHERE "user"."id"=$1 ORDER BY "id" ASC`;
+        const interestsQuery = `SELECT "interest"."id", "interest"."user_id", "active", "address", "unit_number", "state", "city", "zip_code", "property_type",
+     "net_operating_income", "gross_income", "gross_expense", "desired_price" FROM "property" JOIN "interest" ON "property"."id"="interest"."property_id" 
+     JOIN "user" ON "interest"."user_id"= "user"."id" WHERE "user"."id"=$1 ORDER BY "id" ASC`;
         //check to see if a specific property is already favorited
-        const propertyInterests = await pool.query(propertyInterestsQuery, [userId]);
+        const favorites = await pool.query(favoritesQuery, [userId]);
         const interests = await pool.query(interestsQuery, [userId]);
-        res.send({ interests: interests.rows, favorites: propertyInterests.rows});
+        res.send({ interests: interests.rows, favorites: favorites.rows });
     } catch (error) {
-        console.log('post favorite error', error);
+        console.log('get favorite error', error);
         res.sendStatus(500);
     }
 });
