aa411c6c81d64bc3d443e04537949aad9b53c287
Matthew Kimlinger <57105142+mattkimlinger@users.noreply.github.com>
2020-04-24 10:46:59 -0500

    Revert "fixed get more information routing, deleted "home" button"

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,51 +4,47 @@ import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 
-const Nav = (props) => {
-  return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">OTTO</h2>
+const Nav = (props) => (
+  <div className="nav">
+    <Link to="/home">
+      <h2 className="nav-title">OTTO</h2>
+    </Link>
+    <div className="nav-right">
+      <Link className="nav-link" to="/admin/property">Admin Properties</Link>
+      <Link className="nav-link" to="/admin/user">Admin Users</Link>
+      <Link className="nav-link" to="/home"
+        onClick={() => {
+          console.log('login clicked');
+          props.dispatch({ type: 'LOGIN_REGISTER_MODAL_OPEN', payload: true})
+        }}
+      >
+        {/* Show this link if they are logged in or not,
+        but call this link 'Home' if they are logged in,
+        and call this link 'Login / Register' if they are not */}
+        { props.user.id ? 'Home' : 'Login / Register' }
       </Link>
-      <div className="nav-right">
-        <Link className="nav-link" to="/admin/property">Admin Properties</Link>
-        <Link className="nav-link" to="/admin/user">Admin Users</Link>
-        {/* Show the link to the For Sale/ Add Listing/ Account Page and the Log Out if the user is logged in */}
-        {!props.user.id && (
-          <Link className="nav-link" to="/home"
-            onClick={() => {
-              console.log('login clicked');
-              props.dispatch({ type: 'LOGIN_REGISTER_MODAL_OPEN', payload: true })
-            }}
-          >
-            {'Login / Register'}
+      {/* Show the link to the For Sale/ Add Listing/ Account Page and the Log Out if the user is logged in */}
+      {props.user.id && (
+        <>
+          <Link className="nav-link" to="/ForSalePage">
+            For Sale
           </Link>
-        )}
-        {props.user.id && (
-          <>
-            <Link className="nav-link" to="/ForSalePage">
-              For Sale
+          <Link className="nav-link" to="/PropertyInputPage">
+            Add Listing
           </Link>
-            <Link className="nav-link" to="/PropertyInputPage">
-              Add Listing
+          <Link className="nav-link" to="/account">
+            Account Page
           </Link>
-            <Link className="nav-link" to="/account">
-              Account Page
-          </Link>
-            <LogOutButton className="nav-link" />
-          </>
-        )}
-        {/* Always show this link since the about page is not protected */}
-        {/* <Link className="nav-link" to="/LandingPage">
+          <LogOutButton className="nav-link" />
+        </>
+      )}
+      {/* Always show this link since the about page is not protected */}
+      {/* <Link className="nav-link" to="/LandingPage">
         Initial Home Page
       </Link> */}
-      </div>
     </div>
-  )
-
-};
-
-
+  </div>
+);
 
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
