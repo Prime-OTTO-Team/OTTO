de53ba609a329c23be4765710988df54e399fa10
Kyle Greene <kylengreene@gmail.com>
2020-04-26 14:49:50 -0500

    changed home nav link to log out button

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -13,17 +13,7 @@ const Nav = (props) => (
     <div className="nav-right">
       <Link className="nav-link" to="/admin/property">Admin Properties</Link>
       <Link className="nav-link" to="/admin/user">Admin Users</Link>
-      <Link className="nav-link" to="/home"
-        onClick={() => {
-          console.log('login clicked');
-          props.dispatch({ type: 'LOGIN_REGISTER_MODAL_OPEN', payload: true})
-        }}
-      >
-        {/* Show this link if they are logged in or not,
-        but call this link 'Home' if they are logged in,
-        and call this link 'Login / Register' if they are not */}
-        { props.user.id ? 'Home' : 'Login / Register' }
-      </Link>
+     
       {/* Show the link to the For Sale/ Add Listing/ Account Page and the Log Out if the user is logged in */}
       {props.user.id && (
         <>
@@ -36,9 +26,20 @@ const Nav = (props) => (
           <Link className="nav-link" to="/account">
             Account Page
           </Link>
-          <LogOutButton className="nav-link" />
+          
         </>
       )}
+      <Link className="nav-link" to="/home"
+        onClick={() => {
+          console.log('login clicked');
+          props.dispatch({ type: 'LOGIN_REGISTER_MODAL_OPEN', payload: true })
+        }}
+      >
+        {/* Show this link if they are logged in or not,
+        but call this link 'Home' if they are logged in,
+        and call this link 'Login / Register' if they are not */}
+        {props.user.id ? <LogOutButton className="" /> : 'Login / Register'}
+      </Link>
       {/* Always show this link since the about page is not protected */}
       {/* <Link className="nav-link" to="/LandingPage">
         Initial Home Page
